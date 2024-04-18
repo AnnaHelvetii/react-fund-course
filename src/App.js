bcf5abad0dd1a27e008fc40cc1c1017c26ab0259
Anna Soboleva <sithhappens1138@gmail.com>
2024-04-18 14:59:13 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Link, Route, Routes, Navigate } from "react-router-dom";
+import Navbar from "./Components/UI/navbar/Navbar";
+import AppRouter from "./Components/AppRouter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Navbar/>
+			<AppRouter/>
+		</BrowserRouter>
+	)
 }
 
 export default App;
