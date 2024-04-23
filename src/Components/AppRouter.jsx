4b6163ed70ae1784f1ad5c54decf2996f6db9ece
Anna Soboleva <sithhappens1138@gmail.com>
2024-04-23 20:07:56 +0300

    Better navigation

--- src/Components/AppRouter.jsx
+++ src/Components/AppRouter.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import About from "../pages/About";
-import Posts from "../pages/Posts";
-import Error from "../pages/Error";
-import PostIdPage from "../pages/PostIdPage";
+import { publicRoutes, privateRoutes } from "../router/routes";
 
 const AppRouter = () => {
+	const isAuth = true;
 	return (
-		<Routes>
-			<Route path="/about" element={<About />} />
-			<Route exact path="/posts" element={<Posts />} />
-			<Route exact path="/posts/:id" element={<PostIdPage />} />
-			<Route path="/error" element={<Error/>}/>
-			<Route path="*" element={<Navigate replace to="error"/>} />
-		</Routes>
+		isAuth
+			?
+			<Routes>
+				{privateRoutes.map((route, i) => 
+					<Route
+						key={route.path}
+						path={route.path}
+						element={<route.component/>}
+						exact={route.exact}
+					/>
+					
+				)}
+				<Route path="/*" element={<Navigate replace to="posts"/>} />
+			</Routes>
+			:
+			<Routes>
+				{publicRoutes.map((route, i) => 
+					<Route
+						key={route.path}
+						path={route.path}
+						element={<route.component/>}
+						exact={route.exact}
+					/>
+					
+				)}
+				<Route path="/*" element={<Navigate replace to="login"/>} />
+			</Routes>
 	);
 };
 
