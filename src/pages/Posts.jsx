de38c98ac0d21a5042d7a4415c39a98d1ea07956
Anna Soboleva <sithhappens1138@gmail.com>
2024-04-25 15:35:40 +0300

    Dynamic Pagination. useObserver

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -11,69 +11,87 @@ import Loader from "../Components/UI/Loader/Loader";
 import {useFetching} from "../hooks/useFetching";
 import {getPageCount, getPagesArray} from "../utils/pages";
 import Pagination from "../Components/UI/pagination/Pagination";
+import { useObserver } from "../hooks/useObserver";
+import MySelect from "../Components/UI/select/MySelect";
 
 function Posts() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({sort: '', query: ''});
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+	const [posts, setPosts] = useState([]);
+	const [filter, setFilter] = useState({sort: '', query: ''});
+	const [modal, setModal] = useState(false);
+	const [totalPages, setTotalPages] = useState(0);
+	const [limit, setLimit] = useState(10);
+	const [page, setPage] = useState(1);
+	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+	const lastElement = useRef();
 
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
-      const response = await PostService.getAll(limit, page);
-      setPosts(response.data)
-      const totalCount = response.headers['x-total-count']
-      setTotalPages(getPageCount(totalCount, limit));
-  })
+	const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+		const response = await PostService.getAll(limit, page);
+		setPosts([...posts, ...response.data])
+		const totalCount = response.headers['x-total-count']
+		setTotalPages(getPageCount(totalCount, limit));
+	})
 
-  console.log(totalPages)
+	useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+		setPage(page + 1);
+	})
 
-  useEffect(() => {
-    fetchPosts(limit, page)
-  }, []);
 
-  const createPost = (newPost) => {
+	useEffect(() => {
+		fetchPosts(limit, page)
+	}, [page, limit]);
+
+	const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
-  }
+	}
 
-  const removePost = (post) => {
+	const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
-  }
+	}
 
-  const changePage = (page) => {
-      setPage(page)
-      fetchPosts(limit, page)
-  }
+	const changePage = (page) => {
+		setPage(page)
+	}
 
-  return (
-      <div className="App">
-          <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-              Создать пользователя
-          </MyButton>
-          <MyModal visible={modal} setVisible={setModal}>
-              <PostForm create={createPost}/>
-          </MyModal>
-          <hr style={{margin: '15px 0'}}/>
-          <PostFilter
-            filter={filter}
+	return (
+		<div className="App">
+			<MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+				Создать пользователя
+			</MyButton>
+			<MyModal visible={modal} setVisible={setModal}>
+				<PostForm create={createPost}/>
+			</MyModal>
+			<hr style={{margin: '15px 0'}}/>
+			<PostFilter
+			filter={filter}
             setFilter={setFilter}
-          />
-          {postError &&
-            <h1>Произошла ошибка &{postError}</h1>}
-          {isPostsLoading
-              ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
-              : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
-          }
-          <Pagination
-              page={page}
-              changePage={changePage}
-              totalPages={totalPages}
-          />
-      </div>
-  );
+			/>
+			<MySelect
+				value={limit}
+				onChange={value => setLimit(value)}
+				defaultValue= "Кол-во элементов на странице"
+				options={[
+					{value: 5, name: '5'},
+					{value: 10, name: '10'},
+					{value: 25, name: '25'},
+					{value: -1, name: 'Показать все'}
+				]}
+			/>
+			{postError &&
+				<h1>Произошла ошибка &{postError}</h1>
+			}
+			<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+			<div ref={lastElement} style={{height: 20, background: 'red'}} />
+			{isPostsLoading &&
+				<div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+			}
+			<Pagination
+				page={page}
+				changePage={changePage}
+				totalPages={totalPages}
+			/>
+		</div>
+	);
 }
 
 export default Posts;
